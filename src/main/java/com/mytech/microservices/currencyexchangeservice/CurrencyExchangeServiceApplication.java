7fd4be8828de2057669cf332ded43e2c20195640
Maycem_AMOR <maycem.amor@gmail.com>
2024-04-17 20:06:34 +0200

    - Setup Currency Exchange & Conversion Microservices - Kubernetes

--- src/main/java/com/mytech/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ src/main/java/com/mytech/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -2,11 +2,9 @@ package com.mytech.microservices.currencyexchangeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class CurrencyExchangeServiceApplication {
 
     public static void main(String[] args) {
